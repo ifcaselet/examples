36c370ef3f4afb66bc46c894b2191b5c3db3486b
ifcaselet <ifcaselet@gmail.com>
2021-01-01 17:40:33 -0700

    Add IBOutlets

--- Mirroring/Mirroring/ViewController.swift
+++ Mirroring/Mirroring/ViewController.swift
@@ -3,11 +3,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberOfPlayersLabel: UILabel!
+    @IBOutlet weak var purchaseButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
