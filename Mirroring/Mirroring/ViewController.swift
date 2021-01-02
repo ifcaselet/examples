bc858c88a2ddf36ffc19c56d942b8eab81629446
ifcaselet <ifcaselet@gmail.com>
2021-01-01 17:41:08 -0700

    Make IBOutlets private

--- Mirroring/Mirroring/ViewController.swift
+++ Mirroring/Mirroring/ViewController.swift
@@ -3,9 +3,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var numberOfPlayersLabel: UILabel!
-    @IBOutlet weak var purchaseButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var numberOfPlayersLabel: UILabel!
+    @IBOutlet private weak var purchaseButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
