17ccde1280a6b3de0a835ee39cd7f490b1a93fc6
ifcaselet <ifcaselet@gmail.com>
2021-01-10 19:26:31 -0700

    Rename AppTests to MigrationTests

--- UnitTestingCoreDataMigration/AppTests/MigrationTests.swift
+++ UnitTestingCoreDataMigration/AppTests/MigrationTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import App
 
-class AppTests: XCTestCase {
+final class MigrationTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
