6eb6c79d1b3f99a31acb94dd9c6ed40043f22222
ifcaselet <ifcaselet@gmail.com>
2021-01-17 06:36:13 -0700

    Add CoreData Utils

---
+++ UnitTestingCoreDataMigration/AppTests/CoreDataUtils.swift
@@ -0,0 +1,40 @@
+import CoreData
+import XCTest
+
+@testable import App
+
+private let momdURL = Bundle(for: ViewController.self).url(forResource: "App", withExtension: "momd")!
+
+func startPersistentContainer(_ versionName: String) throws -> NSPersistentContainer {
+    let storeURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
+        .appendingPathComponent(UUID().uuidString)!
+        .appendingPathExtension("sqlite")
+
+    let model = managedObjectModel(versionName: versionName)
+
+    let container = makePersistentContainer(storeURL: storeURL, managedObjectModel: model)
+    container.loadPersistentStores { _, error in
+        XCTAssertNil(error)
+    }
+
+    return container
+}
+
+func makePersistentContainer(storeURL: URL,
+                             managedObjectModel: NSManagedObjectModel) -> NSPersistentContainer {
+    let description = NSPersistentStoreDescription(url: storeURL)
+    // Do not automatically migrate because we will be manually migrating (and testing) the store.
+    description.shouldMigrateStoreAutomatically = false
+    description.type = NSSQLiteStoreType
+
+    let container = NSPersistentContainer(name: "App Container", managedObjectModel: managedObjectModel)
+    container.persistentStoreDescriptions = [description]
+
+    return container
+}
+
+func managedObjectModel(versionName: String) -> NSManagedObjectModel {
+    let url = momdURL.appendingPathComponent(versionName).appendingPathExtension("mom")
+    return NSManagedObjectModel(contentsOf: url)!
+}
+
