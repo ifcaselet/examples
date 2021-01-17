fc8f6231ac09efbe37f87594b2cded22ef19f803
ifcaselet <ifcaselet@gmail.com>
2021-01-17 11:49:27 -0700

    Add documentation for CoreDataUtils

--- UnitTestingCoreDataMigration/AppTests/CoreDataUtils.swift
+++ UnitTestingCoreDataMigration/AppTests/CoreDataUtils.swift
@@ -6,6 +6,11 @@ import XCTest
 private let momdURL = Bundle(for: ViewController.self).url(forResource: "App", withExtension: "momd")!
 private let storeType = NSSQLiteStoreType
 
+/// Create and load a store using the given model version. The store will be located in a
+/// temporary directory.
+///
+/// - Parameter versionName: The name of the model (`.xcdatamodel`). For example, `"App V1"`.
+/// - Returns: An `NSPersistentContainer` that is loaded and ready for usage.
 func startPersistentContainer(_ versionName: String) throws -> NSPersistentContainer {
     let storeURL = makeTemporaryStoreURL()
     let model = managedObjectModel(versionName: versionName)
@@ -19,18 +24,34 @@ func startPersistentContainer(_ versionName: String) throws -> NSPersistentConta
     return container
 }
 
+/// Migrates the given `container` to a new store URL. The new (migrated) store will be located
+/// in a temporary directory.
+///
+/// - Parameter container: The `NSPersistentContainer` containing the source store that will be
+///                        migrated.
+/// - Parameter versionName: The name of the model (`.xcdatamodel`) to migrate to. For example,
+///                          `"App V2"`.
+///
+/// - Returns: A migrated `NSPersistentContainer` that is loaded and ready for usage. This
+///            container uses a different store URL than the original `container`.
 func migrate(container: NSPersistentContainer, to versionName: String) throws -> NSPersistentContainer {
+    // Define the source and destination `NSManagedObjectModels`.
     let sourceModel = container.managedObjectModel
     let destinationModel = managedObjectModel(versionName: versionName)
 
     let sourceStoreURL = storeURL(from: container)
+    // Create a new temporary store URL. This is where the migrated data using the model
+    // will be located.
     let destinationStoreURL = makeTemporaryStoreURL()
 
+    // Infer a mapping model between the source and destination `NSManagedObjectModels`.
+    // Modify this line if you use a custom mapping model.
     let mappingModel = try NSMappingModel.inferredMappingModel(forSourceModel: sourceModel,
                                                                destinationModel: destinationModel)
 
     let migrationManager = NSMigrationManager(sourceModel: sourceModel,
                                               destinationModel: destinationModel)
+    // Migrate the `sourceStoreURL` to `destinationStoreURL`.
     try migrationManager.migrateStore(from: sourceStoreURL,
                                       sourceType: storeType,
                                       options: nil,
@@ -39,6 +60,7 @@ func migrate(container: NSPersistentContainer, to versionName: String) throws ->
                                       destinationType: storeType,
                                       destinationOptions: nil)
 
+    // Load the store at `destinationStoreURL` and return the container.
     let destinationContainer = makePersistentContainer(storeURL: destinationStoreURL,
                                                        managedObjectModel: destinationModel)
     destinationContainer.loadPersistentStores { _, error in
