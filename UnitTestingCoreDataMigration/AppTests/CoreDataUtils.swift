a1bbeee5e39bdbee172c65bfa864e1319e0f91cf
ifcaselet <ifcaselet@gmail.com>
2021-01-17 08:12:29 -0700

    Add migrate() helper for migrating to another model

--- UnitTestingCoreDataMigration/AppTests/CoreDataUtils.swift
+++ UnitTestingCoreDataMigration/AppTests/CoreDataUtils.swift
@@ -10,12 +10,42 @@ func startPersistentContainer(_ versionName: String) throws -> NSPersistentConta
     let storeURL = makeTemporaryStoreURL()
     let model = managedObjectModel(versionName: versionName)
 
-    let container = makePersistentContainer(storeURL: storeURL, managedObjectModel: model)
+    let container = makePersistentContainer(storeURL: storeURL,
+                                            managedObjectModel: model)
     container.loadPersistentStores { _, error in
         XCTAssertNil(error)
     }
 
     return container
+}
+
+func migrate(container: NSPersistentContainer, to versionName: String) throws -> NSPersistentContainer {
+    let sourceModel = container.managedObjectModel
+    let destinationModel = managedObjectModel(versionName: versionName)
+
+    let sourceStoreURL = storeURL(from: container)
+    let destinationStoreURL = makeTemporaryStoreURL()
+
+    let mappingModel = try NSMappingModel.inferredMappingModel(forSourceModel: sourceModel,
+                                                               destinationModel: destinationModel)
+
+    let migrationManager = NSMigrationManager(sourceModel: sourceModel,
+                                              destinationModel: destinationModel)
+    try migrationManager.migrateStore(from: sourceStoreURL,
+                                      sourceType: storeType,
+                                      options: nil,
+                                      with: mappingModel,
+                                      toDestinationURL: destinationStoreURL,
+                                      destinationType: storeType,
+                                      destinationOptions: nil)
+
+    let destinationContainer = makePersistentContainer(storeURL: destinationStoreURL,
+                                                       managedObjectModel: destinationModel)
+    destinationContainer.loadPersistentStores { _, error in
+        XCTAssertNil(error)
+    }
+
+    return destinationContainer
 }
 
 private func makePersistentContainer(storeURL: URL,
@@ -34,6 +64,11 @@ private func makePersistentContainer(storeURL: URL,
 private func managedObjectModel(versionName: String) -> NSManagedObjectModel {
     let url = momdURL.appendingPathComponent(versionName).appendingPathExtension("mom")
     return NSManagedObjectModel(contentsOf: url)!
+}
+
+private func storeURL(from container: NSPersistentContainer) -> URL {
+    let description = container.persistentStoreDescriptions.first!
+    return description.url!
 }
 
 private func makeTemporaryStoreURL() -> URL {
