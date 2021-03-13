e2ac885a40e96c510ce23b582dc37383075b0463
ifcaselet <ifcaselet@gmail.com>
2021-03-13 10:35:54 -0700

    Self-dismiss AnotherViewController

--- RemoveCancellableAfterSink/App/AnotherViewController.swift
+++ RemoveCancellableAfterSink/App/AnotherViewController.swift
@@ -1,8 +1,25 @@
 import UIKit
+import Combine
+
+struct SingletonTimerProvider {
+    static let timer = Timer.publish(every: 5, on: .main, in: .default).autoconnect()
+}
 
 final class AnotherViewController: UIViewController {
+
+    private var cancellables = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .purple
+
+        SingletonTimerProvider.timer.sink { value in
+            self.dismiss(animated: true, completion: nil)
+        }.store(in: &cancellables)
     }
+
+    deinit {
+        print("deinit")
+    }
+
 }
