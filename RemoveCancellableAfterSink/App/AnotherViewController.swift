76e3b61d7d7881c79dbec38eb726a414545e1f4e
ifcaselet <ifcaselet@gmail.com>
2021-03-14 16:42:48 -0600

    Making the problem more complex

--- RemoveCancellableAfterSink/App/AnotherViewController.swift
+++ RemoveCancellableAfterSink/App/AnotherViewController.swift
@@ -2,37 +2,44 @@ import UIKit
 import Combine
 
 struct SingletonTimerProvider {
-    static let timer = Timer.publish(every: 3, on: .main, in: .default).autoconnect()
+    static let timer = Timer.publish(every: 2, on: .main, in: .default).autoconnect()
 }
 
 final class AnotherViewController: UIViewController {
 
     private var cancellables = Set<AnyCancellable>()
+    private let startingTimeInterval = Date().timeIntervalSince1970
+    private let maxThreshold = 10.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
 
-        var cancellable: AnyCancellable?
-        cancellable = SingletonTimerProvider.timer.sink { [weak self, weak cancellable] value in
-            print("received timer value: \(value)")
+        subscribeToTimer(threshold: 3.0)
+    }
 
+    private func subscribeToTimer(threshold: TimeInterval) {
+        SingletonTimerProvider.timer.sink { [weak self] value in
+            print("Received timer value: \(value)")
             guard let self = self else {
                 return
             }
 
-            if let cancellable = cancellable {
-                self.cancellables.remove(cancellable)
+            print("Subscriptions = \(self.cancellables.count)")
+
+            let threshold = min(threshold, self.maxThreshold)
+            if threshold == self.maxThreshold {
+                print("Max threshold reached. There should be no active subscriptions after this.")
             }
 
-            self.dismiss(animated: true, completion: nil)
-        }
-
-        cancellables.insert(cancellable!)
+            if value.timeIntervalSince1970 - self.startingTimeInterval < min(threshold, self.maxThreshold) {
+                print("Recursively subscribing to timer")
+                self.subscribeToTimer(threshold: threshold + 3.0)
+            }
+        }.store(in: &cancellables)
     }
 
     deinit {
         print("deinit")
     }
-
 }
