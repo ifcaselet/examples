6db9a4d82b4568a6df06b1a14ae387382e8fdf88
ifcaselet <ifcaselet@gmail.com>
2021-05-26 09:25:41 -0600

    Add button, label, and activity indicator

--- CoreDataStack/App/PerformanceTestViewController.swift
+++ CoreDataStack/App/PerformanceTestViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 final class PerformanceTestViewController: UIViewController {
+
+    @IBOutlet private weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet private weak var statusLabel: UILabel!
+
+    private let stack = CoreDataStack()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("loaded!")
+        statusLabel.text = ""
+    }
+
+    @IBAction func executePerformanceTest() {
+
     }
 }
